9be5e280b086148b0cdeac5f317d633682ea0b04
Manuel Romero <mjromper@gmail.com>
2023-03-13 13:56:19 +0100

    fix

--- src/s3.js
+++ src/s3.js
@@ -4,8 +4,6 @@ const s3 = new AWS.S3();
 
 async function  store( fileKey, data ) {
     let key = `datafiles/${fileKey}`;
-    console.log("data to store", data, key);
-    console.log("stringify data", JSON.stringify(data, null, 2));
     // store something
     var result = await s3.putObject({
         Body: JSON.stringify(data, null, 2),
@@ -18,15 +16,12 @@ async function  store( fileKey, data ) {
 
 async function  read( fileKey, isArray ) {
     let key = `datafiles/${fileKey}`;
-
-    console.log("reading key", key);
     try {
         let my_file = await s3.getObject({
             Bucket: process.env.CYCLIC_BUCKET_NAME,
             Key: key,
         }).promise();
 
-        console.log("my_file", my_file.Body.toString());
         return JSON.parse(my_file.Body.toString())
     } catch (e) {
         console.log("error", e);
